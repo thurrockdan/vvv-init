9f164342c37a433b9e98dd6e4cfe29fab931e59c
Neil Pie <neil@neilpie.co.uk>
2014-07-27 00:37:12 +0100

    working on PIE version

--- vvv-init.sh
+++ vvv-init.sh
@@ -1,73 +1,62 @@
 #!/bin/bash
-# Init script for a development site with a monolithic Git repo
-# v1.0
+SITE_NAME="Human Readable Site Name"
+DB_NAME="database_name"
+SITE_URL="http://local.site-name"
 
-# Edit these variables to suit your porpoises
-# -------------------------------------------
 
-# Just a human readable description of this site
-SITE_NAME="Site Name"
-# The name (to be) used by MySQL for the DB
-DB_NAME="site_name"
-# The repo URL in SSH format, e.g. git@github.com:cftp/foo.git
+echo "Commencing setup for $SITE_NAME"
 
-# ----------------------------------------------------------------
-# You should not need to edit below this point. Famous last words.
+# Run Composer
+echo "Running Composer to download dependencies"
+composer install --prefer-dist
 
-echo "Commencing $SITE_NAME setup"
-
-# Add GitHub.com to known hosts, so we don't get prompted
-# to verify the server fingerprint.
-mkdir -p /root/.ssh
-touch /root/.ssh/known_hosts
-IFS=$'\n'
-for KNOWN_HOST in $(cat "ssh/known_hosts"); do
-	if ! grep -Fxq "$KNOWN_HOST" /root/.ssh/known_hosts; then
-	    echo "Adding host to SSH known_hosts for user 'root': $KNOWN_HOST"
-	    echo $KNOWN_HOST >> /root/.ssh/known_hosts
-	fi
-done
-
-# Reload SSH, to get it to notice the change to known_hosts
-service ssh force-reload
-
-# Clone the repo, if it's not there already
-if [ ! -d htdocs ]
+if [ ! -f public_html/wp-config.php ]
 then
-	ssh-agent bash -c "ssh-add ssh/cftp_deploy_id_rsa; git clone $REPO_SSH_URL htdocs;"
-	echo "Cloning the repo"
-else
-	echo "The htdocs directory already exists, and should contain the repo. If not, delete it and run Vagrant provisioning again."
-fi
+	echo "Creating wp-config.php and moving it up into public_html because we like it there"
 
-# Make a database, if we don't already have one
-mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME; GRANT ALL PRIVILEGES ON $DB_NAME.* TO wp@localhost IDENTIFIED BY 'wp';"
+	wp core config --dbname="$DB_NAME" --dbuser=wp --dbpass=wp --dbhost="localhost" --dbprefix="wp_"  --extra-php <<PHP
+	define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/wp-content' );
+	define( 'WP_CONTENT_URL', '$SITE_URL/wp-content' );
+	define( 'WP_DEBUG', true );
+	define( 'WP_DEBUG_LOG', true );
+	define('WP_DEBUG_DISPLAY', false);
+	define('SAVEQUERIES', true);
 
-# Let's get some config in the house
-if [ ! -f htdocs/wp-config.php ]; then
-	echo "Creating wp-config.php"
-	 wp core config --dbname="$DB_NAME" --dbuser=wp --dbpass=wp --dbhost="localhost" --extra-php <<PHP
-$EXTRA_CONFIG
 PHP
-
+mv public_html/wp/wp-config.php public_html/wp-config.php
 else
 	echo "wp-config.php already exists"
 fi
 
-DATA_IN_DB=`mysql -u root --password=root --skip-column-names -e "SHOW TABLES FROM $DB_NAME;"`
-if [ "" == "$DATA_IN_DB" ]; then
-	if [ ! -f initial-data.sql ]
-	then
-		echo "DATABASE NOT INSTALLED, add initial-data.sql file and run Vagrant provisioning again"
-	else
-		echo "Loading the database with lovely data"
-		mysql -u root --password=root $DB_NAME < initial-data.sql
-	fi
-	wp user create dev_admin dev_admin@example.com --role=administrator --user_pass=password
+# Move index.php and change path to blog-header so that we can run WordPress in a sub-directory
+if [ ! -f public_html/index.php ]; then
+
+	echo "Copying index.php and moving it up into public_html because we like it there"
+
+	cp public_html/wp/index.php public_html/index.php
+	sed -i -e "s/wp-blog-header.php/wp\/wp-blog-header.php/g" public_html/index.php
 else
-	echo "Database has data, skipping"
+	echo "index.php already exists"
 fi
+
+# Make a database, if we don't already have one
+echo "Creating database (if it's not already there)"
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO wp@localhost IDENTIFIED BY 'wp';"
+
+if [ -f $DB_NAME.sql ]; then
+	echo "Importing DB content from dump file"
+
+	wp db import $DB_NAME.sql
+
+elif ! $(wp core is-installed ); then
+	echo "Installing initial WordPress DB tables"
+	wp core install --title=$SITE_NAME --admin_user=admin --admin_password=password --admin_email=admin@no.reply
+	wp option update siteurl "$SITE_URL\/wp"
+fi
+
+
 
 # The Vagrant site setup script will restart Nginx for us
 
-echo "$SITE_NAME site now installed, you may want to add the user uploaded files";
+echo "$SITE_NAME site now installed";
