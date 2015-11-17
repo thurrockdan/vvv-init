b0bb7367967330ef0f5241d00bd27597ce71f401
Daniel Smith <daniel@ecomstudio.co.uk>
2015-11-17 15:22:42 +0000

    Update vvv-init.sh

--- vvv-init.sh
+++ vvv-init.sh
@@ -19,33 +19,7 @@ EXTRA_CONFIG="
 echo "---------------------------"
 echo "Commencing $SITE_NAME setup"
 
-# Add GitHub and GitLab to known_hosts, so we don't get prompted
-# to verify the server fingerprint.
-# The fingerprints in [this repo]/ssh/known_hosts are generated as follows:
-#
-# As the starting point for the ssh-keyscan tool, create an ASCII file
-# containing all the hosts from which you will create the known hosts
-# file, e.g. sshhosts.
-# Each line of this file states the name of a host (alias name or TCP/IP
-# address) and must be terminated with a carriage return line feed
-# (Shift + Enter), e.g.
-#
-# bitbucket.org
-# github.com
-# gitlab.com
-#
-# Execute ssh-keyscan with the following parameters to generate the file:
-#
-# ssh-keyscan -t rsa,dsa -f ssh_hosts >ssh/known_hosts
-# The parameter -t rsa,dsa defines the host’s key type as either rsa
-# or dsa.
-# The parameter -f /home/user/ssh_hosts states the path of the source
-# file ssh_hosts, from which the host names are read.
-# The parameter >ssh/known_hosts states the output path of the
-# known_host file to be created.
-#
-# From "Create Known Hosts Files" at:
-# http://tmx0009603586.com/help/en/entpradmin/Howto_KHCreate.html
+
 mkdir -p ~/.ssh
 touch ~/.ssh/known_hosts
 IFS=$'\n'
@@ -105,22 +79,6 @@ if [ ! -f public_html/index.php ]; then
 else
 	echo "index.php already exists"
 fi
-# Load the composer stuff
-#./wrapper-composer.sh update
-
-# DATA_IN_DB=`mysql -u root --password=root --skip-column-names -e "SHOW TABLES FROM $DB_NAME;"`
-# if [ "" == "$DATA_IN_DB" ]; then
-# 	if [ ! -f initial-data.sql ]
-# 	then
-# 		echo "DATABASE NOT INSTALLED, add initial-data.sql file and run Vagrant provisioning again"
-# 	else
-# 		echo "Loading the database with lovely data"
-# 		mysql -u root --password=root $DB_NAME < initial-data.sql
-# 	fi
-# 	wp user create dev_admin dev_admin@example.com --role=administrator --user_pass=password
-# else
-# 	echo "index.php already exists"
-# fi
 
 # Make a database, if we don't already have one
 DATA_IN_DB=`mysql -u root --password=root --skip-column-names -e "SHOW TABLES FROM $DB_NAME;"`
